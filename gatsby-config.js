12bd405bb4441b8748d486843b93b246375342f1
benaber1990 <benaber1990@gmail.com>
2023-06-15 19:00:32 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `golden-goal`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
